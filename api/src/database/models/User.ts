fc7a51a728ee4461628db78fb05af0364550b6cf
Stuart <dickie.stuart@gmail.com>
2021-03-06 16:55:51 -0500

    refactored auth process to include google signin

--- api/src/database/models/User.ts
+++ api/src/database/models/User.ts
@@ -1,73 +1,95 @@
-import bcrypt from 'bcrypt';
-import crypto from 'crypto';
-
-export enum DbUserActiveStatus {
-    unverified = 0,
-    active,
-    disabled
-}
-
+// import bcrypt from 'bcrypt';
+// import crypto from 'crypto';
 export interface DbUser {
     _id: string;
-    email: string;
-    pass: string;
+    googleId: string;
     createdOn: Date;
     updatedOn: Date;
-    active: DbUserActiveStatus;
-    verifyEmail: string | null;
-    verifyEmailCreatedOn: Date | null;
-    verifyPass: string | null;
-    verifyPassCreatedOn: Date | null;
 }
 
-type CreateDbUser = Omit<DbUser, '_id'>;
-export type DbUserAuthCredentials = Pick<DbUser, 'email' | 'pass'>;
+export type DbUserAuthCredentials = Pick<DbUser, 'googleId'>;
 
 export default class User {
 
-    public static async encrypt (value: string): Promise<string> {
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(value, salt);
+    public static create (args: DbUserAuthCredentials): Omit<DbUser, '_id'> {
+        const currentDate = new Date();
 
-        return hash;
-    }
-
-    public static async validatePassword (hash: string, password: string): Promise<boolean> {
-        try {
-            return await bcrypt.compare(password, hash);
-        } catch (e) {
-            // TODO: log error
-    
-            return false;
+        return {
+            googleId: args.googleId,
+            createdOn: currentDate,
+            updatedOn: currentDate
         }
     }
-    
-    public static async create (args: DbUserAuthCredentials): Promise<CreateDbUser | null> {
-        try {
-            const hash = await this.encrypt(args.pass);
-    
-            const currentDate = new Date();
-    
-            const verifyToken = crypto.randomBytes(256).toString('hex');
-            const verifyExp = currentDate;
-    
-            return {
-                email: args.email,
-                pass: hash,
-                createdOn: currentDate, 
-                updatedOn: currentDate,
-                active: DbUserActiveStatus.unverified,
-                verifyEmail: verifyToken,
-                verifyEmailCreatedOn: verifyExp,
-                verifyPass: null,
-                verifyPassCreatedOn: null
-            };
-    
-        } catch (e) {
-            // TODO; log error;
-    
-            return null;
-        }
-    }
-
 }
+
+
+// export enum DbUserActiveStatus {
+//     unverified = 0,
+//     active,
+//     disabled
+// }
+
+// export interface DbUser {
+//     _id: string;
+//     email: string;
+//     pass: string;
+//     createdOn: Date;
+//     updatedOn: Date;
+//     active: DbUserActiveStatus;
+//     verifyEmail: string | null;
+//     verifyEmailCreatedOn: Date | null;
+//     verifyPass: string | null;
+//     verifyPassCreatedOn: Date | null;
+// }
+
+// type CreateDbUser = Omit<DbUser, '_id'>;
+// export type DbUserAuthCredentials = Pick<DbUser, 'email' | 'pass'>;
+
+// export default class User {
+
+//     public static async encrypt (value: string): Promise<string> {
+//         const salt = await bcrypt.genSalt(10);
+//         const hash = await bcrypt.hash(value, salt);
+
+//         return hash;
+//     }
+
+//     public static async validatePassword (hash: string, password: string): Promise<boolean> {
+//         try {
+//             return await bcrypt.compare(password, hash);
+//         } catch (e) {
+//             // TODO: log error
+    
+//             return false;
+//         }
+//     }
+    
+//     public static async create (args: DbUserAuthCredentials): Promise<CreateDbUser | null> {
+//         try {
+//             const hash = await this.encrypt(args.pass);
+    
+//             const currentDate = new Date();
+    
+//             const verifyToken = crypto.randomBytes(256).toString('hex');
+//             const verifyExp = currentDate;
+    
+//             return {
+//                 email: args.email,
+//                 pass: hash,
+//                 createdOn: currentDate, 
+//                 updatedOn: currentDate,
+//                 active: DbUserActiveStatus.unverified,
+//                 verifyEmail: verifyToken,
+//                 verifyEmailCreatedOn: verifyExp,
+//                 verifyPass: null,
+//                 verifyPassCreatedOn: null
+//             };
+    
+//         } catch (e) {
+//             // TODO; log error;
+    
+//             return null;
+//         }
+//     }
+
+// }
--- api/src/server/routes/authRoute.ts
+++ api/src/server/routes/authRoute.ts
@@ -23,9 +23,27 @@ export default function authRoutes (router: Router, dbAccess: AccessConstructor,
                 return next(new Error('Invalid token'));
             }
 
+            const googleId = payload['sub'];
+
+            const user = await dbAccess(Users).findOne({ googleId: googleId });
+            let token: string;
+
+            if (!user) {
+                const newUser = await dbAccess(Users).create({ googleId: googleId });
+
+                if (newUser === null) {
+                    return next(new Error('Uh-oh! Something went wrong'));
+                }
+
+                token = tokenService.generate({ user: newUser._id });
+
+            } else {
+                token = tokenService.generate({ user: user._id });
+            }
+
             return res.status(200).json({ 
                 message: 'Successful Google sign-in',
-                user: payload['sub']
+                token: token
              });
 
         } catch (e) {
@@ -33,102 +51,102 @@ export default function authRoutes (router: Router, dbAccess: AccessConstructor,
         }
     });
 
-    router.post('/signin', async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const user = await dbAccess(Users).verifyPassword({ email: req.body.email, pass: req.body.password });
+    // router.post('/signin', async (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const user = await dbAccess(Users).verifyPassword({ email: req.body.email, pass: req.body.password });
             
-            if (user === null) {
-                return next(new Error('Incorrect username or password'));
-            }
+    //         if (user === null) {
+    //             return next(new Error('Incorrect username or password'));
+    //         }
 
-            const token = tokenService.generate({ userId: user._id });
+    //         const token = tokenService.generate({ userId: user._id });
 
-            return res.status(200).json({ message: 'Login succesful', token });
+    //         return res.status(200).json({ message: 'Login succesful', token });
 
-        } catch (e) {
-            return next(e);
-        }
-    });
+    //     } catch (e) {
+    //         return next(e);
+    //     }
+    // });
 
-    router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const matchedUser = await dbAccess(Users).findOne({ email: req.body.email });
+    // router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const matchedUser = await dbAccess(Users).findOne({ email: req.body.email });
 
-            if (matchedUser !== null) {
-                return next(new Error('User already exists'));
-            }
+    //         if (matchedUser !== null) {
+    //             return next(new Error('User already exists'));
+    //         }
 
-            const newUser = await dbAccess(Users).create({ email: req.body.email, pass: req.body.password });
+    //         const newUser = await dbAccess(Users).create({ email: req.body.email, pass: req.body.password });
 
-            if (newUser === null) {
-                return next(new Error('Oops! Something went wrong'));
-            }
+    //         if (newUser === null) {
+    //             return next(new Error('Oops! Something went wrong'));
+    //         }
 
-            if (newUser.verifyEmail !== null) {
-                mailService.sendVerificationEmail(newUser.email, newUser.verifyEmail);
-            }
+    //         if (newUser.verifyEmail !== null) {
+    //             mailService.sendVerificationEmail(newUser.email, newUser.verifyEmail);
+    //         }
 
-            const token = tokenService.generate({ userId: newUser._id });
+    //         const token = tokenService.generate({ userId: newUser._id });
 
-            return res.status(200).json({ message: 'User created successfuly', token });
+    //         return res.status(200).json({ message: 'User created successfuly', token });
 
-        } catch (e) {
-            return next(e);
-        }
-    });
+    //     } catch (e) {
+    //         return next(e);
+    //     }
+    // });
 
-    router.post('/requestResetPassword', async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const response = await dbAccess(Users).requestPasswordReset({ email: req.body.email });
+    // router.post('/requestResetPassword', async (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const response = await dbAccess(Users).requestPasswordReset({ email: req.body.email });
 
-            if (!response) {
-                throw new Error('Oops! Something went wrong');
-            } 
+    //         if (!response) {
+    //             throw new Error('Oops! Something went wrong');
+    //         } 
 
-            mailService.sendChangePassword(req.body.email, response.token );
+    //         mailService.sendChangePassword(req.body.email, response.token );
 
-            return res.status(200).json({ message: 'Password reset sent' });
+    //         return res.status(200).json({ message: 'Password reset sent' });
             
-        } catch (e) {
-            return next(e);
-        }
-    });
+    //     } catch (e) {
+    //         return next(e);
+    //     }
+    // });
 
-    router.get('/resetPassword/:token', async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const token = req.params.token;
-            const pass = req.query.pass?.toString() || '';
-            const verifyPass = req.query.verifyPass?.toString();
+    // router.get('/resetPassword/:token', async (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const token = req.params.token;
+    //         const pass = req.query.pass?.toString() || '';
+    //         const verifyPass = req.query.verifyPass?.toString();
 
-            if ((!pass || !verifyPass ) || pass !== verifyPass) {
-                throw new Error('Values do not match');
-            }
+    //         if ((!pass || !verifyPass ) || pass !== verifyPass) {
+    //             throw new Error('Values do not match');
+    //         }
 
-            const response = dbAccess(Users).verifyPasswordReset(token, pass);
+    //         const response = dbAccess(Users).verifyPasswordReset(token, pass);
 
-            if (!response) {
-                throw new Error('Bad Request');
-            }
+    //         if (!response) {
+    //             throw new Error('Bad Request');
+    //         }
 
-            return res.status(200).json({ message: 'Password reset successfuly' });
+    //         return res.status(200).json({ message: 'Password reset successfuly' });
 
-        } catch (e) {
-            return next(e);
-        }
-    })
+    //     } catch (e) {
+    //         return next(e);
+    //     }
+    // })
 
-    router.get('/verifyEmail/:token', async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const response = await dbAccess(Users).verifyEmail(req.params.token);
+    // router.get('/verifyEmail/:token', async (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const response = await dbAccess(Users).verifyEmail(req.params.token);
                 
-            if (!response) throw new Error('Invalid token');
+    //         if (!response) throw new Error('Invalid token');
             
-            return res.status(200).json({ message: 'Successfuly verified' });
+    //         return res.status(200).json({ message: 'Successfuly verified' });
 
-        } catch (e) {
-            return next(e);
-        }
-    });
+    //     } catch (e) {
+    //         return next(e);
+    //     }
+    // });
 
     return router;
 }
